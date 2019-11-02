59f2f51694f7645fb4f8b2b10a6d8d6c2a15d2ee
JeffersonSoares99 <jefferson99soares@gmail.com>
2019-11-01 22:20:23 -0300

    bla

--- src/PacoteAtividade02/Atividade01.java
+++ src/PacoteAtividade02/Atividade01.java
@@ -1,19 +1,47 @@
 package PacoteAtividade02;
 
-import javax.swing.*;
-
 public class Atividade01 {
     
     public static void main(String[] args) {
         
         int i,x,resultado;
         
-        for(i=1;i<11;i++){
-            for(x=0;x<11;x++){
-                resultado = i * x;
-                JOptionPane.showMessageDialog(null,i+" X "+x+" = "+resultado);
+        
+        for(i=0;i<11;i++){
+            
+            for(x=1;x<6;x++){
+                System.out.print("| "+x+" X "+i+" = "+i*x);
+                if(i*x<10){
+                    System.out.print("  ");
+                }else{
+                    System.out.print(" ");
+                }
+                if(i<10){
+                    System.out.print(" |");
+                }else{
+                    System.out.print("|");
+                }
             }
+            System.out.println("");
         }
+        for(i=0;i<11;i++){
+            
+            for(x=6;x<11;x++){
+                System.out.print("| "+x+" X "+i+" = "+i*x);
+                if(i*x<10){
+                    System.out.print("  ");
+                }else{
+                    System.out.print(" ");
+                }
+                if(i<10){
+                    System.out.print(" |");
+                }else{
+                    System.out.print("|");
+                }
+            }
+            System.out.println("");
+        }
+
     }
     
 }
